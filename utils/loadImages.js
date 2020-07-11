709e7b40e19c1e263c572ab2e04cb75853c47729
midoghranek <midoghranek@hotmail.com>
2020-07-11 05:36:18 +0200

    insta

--- utils/loadImages.js
+++ utils/loadImages.js
@@ -1,74 +1,83 @@
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
+const { createRemoteFileNode } = require("gatsby-source-filesystem");
 
-function isImageKey (key, imageKeys) {
-  return imageKeys.includes(key)
+function isImageKey(key, imageKeys) {
+  return imageKeys.includes(key);
 }
 
-async function createImageNodes ({
+async function createImageNodes({
   entity,
   createNode,
   createNodeId,
   store,
   cache,
   imageName,
-  imageCacheKey
+  imageCacheKey,
 }) {
-  let fileNode
+  let fileNode;
   try {
     fileNode = await createRemoteFileNode({
-      url: entity.data.url,
+      url:
+        entity.data.media_type === "VIDEO"
+          ? entity.data.thumbnail_url
+          : entity.data.media_url,
       store,
       cache,
       createNode,
-      createNodeId
-    })
+      createNodeId,
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
   if (fileNode) {
     await cache.set(imageCacheKey, {
       fileNodeID: fileNode.id,
-      modified: entity.data.modified
-    })
-    console.log('Image downloaded: ' + imageName)
+      modified: entity.data.modified,
+    });
+    console.log("Image downloaded: " + imageName);
     return {
       ...entity,
       links: {
         ...entity.links,
-        local___NODE: fileNode.id
-      }
-    }
+        local___NODE: fileNode.id,
+      },
+    };
   }
-  return entity
+  return entity;
 }
 
-function extensionIsValid (url) {
-  const ext = url.split('.').pop().split('/')[0]
+function extensionIsValid(url) {
+  const ext = url.split(".").pop().split("/")[0];
   switch (ext) {
-    case 'jpg':
-    case 'jpeg':
-    case 'png':
-      return true
+    case "jpg":
+    case "jpeg":
+    case "png":
+      return true;
     default:
-      return false
+      return false;
   }
 }
 
-async function loadImages ({
-  entities, imageKeys, createNode, createNodeId, store, cache, touchNode
+async function loadImages({
+  entities,
+  imageKeys,
+  createNode,
+  createNodeId,
+  store,
+  cache,
+  touchNode,
 }) {
   return Promise.all(
     entities.map(async (entity) => {
       if (!isImageKey(entity.name, imageKeys) || !entity.data.url) {
-        return Promise.resolve(entity)
+        return Promise.resolve(entity);
       }
       if (!extensionIsValid(entity.data.url)) {
-        console.log(`Image-Extension not valid: ${entity.data.url}`)
-        return Promise.resolve(entity)
+        console.log(`Image-Extension not valid: ${entity.data.url}`);
+        return Promise.resolve(entity);
       }
-      const imageName = entity.data.url.match(/([^/]*)\/*$/)[1]
-      const imageCacheKey = `local-image-${imageName}`
-      const cachedImage = await cache.get(imageCacheKey)
+      const imageName = entity.data.url.match(/([^/]*)\/*$/)[1];
+      const imageCacheKey = `local-image-${imageName}`;
+      const cachedImage = await cache.get(imageCacheKey);
       // If we have cached image and it wasn't modified, reuse
       // previously created file node to not try to redownload
       if (
@@ -76,16 +85,16 @@ async function loadImages ({
         entity.data.modified &&
         entity.data.modified === cachedImage.modified
       ) {
-        const { fileNodeID } = cachedImage
-        touchNode({ nodeId: fileNodeID })
-        console.log('Image from Cache: ' + imageName)
+        const { fileNodeID } = cachedImage;
+        touchNode({ nodeId: fileNodeID });
+        console.log("Image from Cache: " + imageName);
         return Promise.resolve({
           ...entity,
           links: {
             ...entity.links,
-            local___NODE: fileNodeID
-          }
-        })
+            local___NODE: fileNodeID,
+          },
+        });
       }
       return createImageNodes({
         entity,
@@ -94,10 +103,10 @@ async function loadImages ({
         store,
         cache,
         imageName,
-        imageCacheKey
-      })
+        imageCacheKey,
+      });
     })
-  )
+  );
 }
 
-module.exports = loadImages
+module.exports = loadImages;
